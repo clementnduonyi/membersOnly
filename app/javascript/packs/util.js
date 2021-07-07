080bc6000ca161ba0535a2986ff8f124c378d994
Cendevs <cndigitech@gmail.com>
2021-07-07 22:20:13 +0100

    modify

--- app/javascript/packs/util.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello, world!")
-
-function divide() {
-    var txt;
-    txt = document.getElementsById('#text').value;
-    var text = txt.split(".");
-    var str = text.join('.</br>');
-    document.write(str);
-}
